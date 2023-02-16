e4e86d22eb76bc1a52d872448a0b548e452a6bb2
Paulina A C Udeh <udehpaulina93@gmail.com>
2023-02-16 11:51:21 -0800

    5th commit of 0x00-hello_world in alx low level programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - The entry point of our program where the runtime will start executing.
+ *Return: 0 (success)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle!");
+	return (0);
+}
