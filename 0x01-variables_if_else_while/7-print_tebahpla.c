cad527119a8d702e56d14712b0c600852b430b33
Paulina A C Udeh <udehpaulina93@gmail.com>
2023-02-17 12:55:33 -0800

    eight commit of variables in c tasks

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 Always (success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
