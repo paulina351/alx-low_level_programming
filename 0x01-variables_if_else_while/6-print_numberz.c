7464fcc94c3b45e5d06d863a109c2cc117a22b44
Paulina A C Udeh <udehpaulina93@gmail.com>
2023-02-17 12:45:05 -0800

    seventh commit of variables in c tasks

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 Always (success)
+ */
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
