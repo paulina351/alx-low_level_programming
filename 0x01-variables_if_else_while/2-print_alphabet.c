7dd1588d1c858525e4295fb1969cb37820a64c2a
Paulina A C Udeh <udehpaulina93@gmail.com>
2023-02-17 11:03:31 -0800

    third commit of variables in c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ *
+ * Return: 0 Always (success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
