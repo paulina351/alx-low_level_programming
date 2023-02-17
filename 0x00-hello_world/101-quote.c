96021cd36731f3a4fd4c6eddb3196368f6796479
Paulina A C Udeh <udehpaulina93@gmail.com>
2023-02-16 20:25:53 -0800

    9th commit of 0x00-hello_world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unlstd.h>
+/**
+ * Main: is the c program that prints standard line error
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
