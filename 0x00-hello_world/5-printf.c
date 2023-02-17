858968c9c2361310d7dc57f902de1b4ac16b187c
Paulina A C Udeh <udehpaulina93@gmail.com>
2023-02-16 20:06:28 -0800

    8th commit of 0x00-hello world in alx low level programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - is the entry point of the program
+ * Return: 0(Success) 
+ */
+int main(void)
+{
+	printf("with proper grammer,but the outcome is a piece of art,\n");
+	return (0);
+}
