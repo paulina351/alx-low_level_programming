272435c504a7494b0f83b27aa5eafa7277863f25
Paulina A C Udeh <udehpaulina93@gmail.com>
2023-02-23 08:36:07 -0800

    Task 3 of more functions and more nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
