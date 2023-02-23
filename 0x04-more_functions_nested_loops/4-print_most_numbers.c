e64f1db61ab170701de1511b82b1e9d036a6246b
Paulina A C Udeh <udehpaulina93@gmail.com>
2023-02-23 08:50:54 -0800

    Task 4 of more functions and nested loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9 then a new line
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (!(i == '2' || i == '4'))
+			_putchar(i);
+	}
+	_putchar('\n');
+}
