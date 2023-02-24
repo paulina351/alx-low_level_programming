acd3b43a5c1bab4c141be42e2f596eaac3a74996
Paulina A C Udeh <udehpaulina93@gmail.com>
2023-02-24 09:21:00 -0800

    Task 1 of debugging in c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,16 +9,19 @@ int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming :/*(*/\n");
+	printf("Infinite loop incoming :(\n");
 
 	i = 0;
 
-	while (i < 10)
-	{
+	/*
+	 *
+		while (i < 10)
+		{
 		putchar(i);
-	}
+		}
+	*/
 
-	printf("Infinite loop avoid! /*\\o/*/\n");
+	printf("Infinite loop avoid! \\o/\n");
 
 	return (0);
 }
