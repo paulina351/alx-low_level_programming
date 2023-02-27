7b3a64ebafed24ccce414a7e9035f82a27b6574e
Paulina A C Udeh <udehpaulina93@gmail.com>
2023-02-27 07:21:58 -0800

    Task 0 of C pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that takes a pointer to an int as a parameter
+ * @n: input
+ * Return: 0;
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+This directory is named 0x05-Pointers_arrays_strings.
+It consist of a total of 12 tasks.
+**12 mandatory and 2 advanced task
