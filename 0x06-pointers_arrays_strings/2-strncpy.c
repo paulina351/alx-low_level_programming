0462ddcf67095996e4d5927061c9d4d51d371b6d
Paulina A C Udeh <udehpaulina93@gmail.com>
2023-03-02 10:04:08 -0800

    Task 2 of 0x06-pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strncpy - function that copies a string
+ * @dest: destination
+ * @src: source
+ * @n: limit
+ *
+ * Return: value
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x = 0, y = 0;
+
+	while (src[y])
+	{
+		y++
+	}
+
+	while (x < n && src[x])
+	{
+		dest[x] = src[x];
+		x++;
+	}
+
+	while (x < n)
+	{
+		dest[x] = '\0';
+		x++;
+	}
+
+	return (dest);
+}
