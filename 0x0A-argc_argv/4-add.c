4b55b1811559b7ff64127a466c3eb0030f58ad0e
Paulina A C Udeh <udehpaulina93@gmail.com>
2023-03-10 22:18:19 -0800

    Task 4 of 0x0A argc - argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * check_num - string that has the digit
+ * @str: string of array
+ * Return: Alway 0
+ */
+
+int check_num(char *str)
+{
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+
+		count++;
+	}
+	return (1);
+}
+/**
+ * main - add positive number
+ * @argc: count arguments
+ * @argv: vector arguments
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc)
+	{
+		if (check_num(argv[count]))
+		{
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		count++;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
