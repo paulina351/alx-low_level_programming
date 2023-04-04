8342879679dff9b98ff9fe6d7369bd70053afedf
Paulina A C Udeh <udehpaulina93@gmail.com>
2023-04-04 08:20:45 -0700

    Task 10 of more singly linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *delete, *select = *head;
 	unsigned int list;
 
-	if (copy == NULL)
+	if (select == NULL)
 		return (-1);
 
 	if (index == 0)
@@ -34,4 +34,4 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	select->next = delete->next;
 	free(delete);
 	return (1);
-}	
+}
