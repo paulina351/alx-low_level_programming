5e9eb0d128f9122d0d1986eb29b9cbdbda2a72c2
Paulina A C Udeh <udehpaulina93@gmail.com>
2023-04-05 14:09:24 -0700

    Task 3 of 0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,43 +9,29 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int a = 0, b = 0, c = 0, r = 0, lim;
+	int i = 0, diff = 0;
 
-	while
-	(s1[a]);
+	while (1)
 	{
-		a++;
-	}
-
-	while (s2[b])
-	{
-		b++;
-	}
-
-	if (a <= b)
-	{
-		lim = a;
-	}
-	else
-	{
-		lim = b;
-	}
-
-	while (c <= lim)
-	{
-		if (s1[c] == s2[c])
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		else if (s1[i] == '\0')
 		{
-			c++;
-			continue;
-		}
-		else
-		{
-			r = s1[c] - s2[c];
+			diff = s2[i];
 			break;
 		}
-
-		c++;
+		else if (s2[i] == '\0')
+		{
+			diff = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
 	}
-
-	return (r);
+	return (diff);
 }
