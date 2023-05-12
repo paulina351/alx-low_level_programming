3adc7a0b0fb17a63cb916da6b2a3e9f05146eaeb
paulina udeh <udehpaulina93@gmail.com>
2023-05-12 06:48:58 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include <unlstd.h>
+#include <unistd.h>
 /**
- * Main - is the entry point of the c program
+ * main - is the entry point of the c program
  * Return: 1 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar,
+		2015-10-19\n", stdout);
 return (1);
 }
