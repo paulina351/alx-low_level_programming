6d8a8184f7beed9419179c72c93149c2bbfb6e3e
Paulina Udeh <udehpaulina93@gmail.com>
2023-08-17 09:22:18 -0700

    add node to double

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,15 +13,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	add_node = malloc(sizeof(dlistint_t));
 
-	if (add_node)
-	{
-		add_node->n = n;
-		add_node->next = *head;
-	}
-	else
+	if (add_node == NULL)
 		return (NULL);
-	if (*head)
-		add_node->next = *head;
+
+	add_node->n = n;
+	add_node->prev = NULL;
+	add_node->next = *head;
+	if (*head != NULL)
+		(*head)->prev = add_node;
 	*head = add_node;
+
 	return (add_node);
 }
