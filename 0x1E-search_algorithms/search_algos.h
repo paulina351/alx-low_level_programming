1df4499c67cd4408ca2c65e6c23b764b9b8c4aa5
Paulina Udeh <udehpaulina93@gmail.com>
2023-12-13 19:14:35 +0000

    header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,13 +5,6 @@
 #include <stdlib.h>
 #include <math.h>
 
-int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
-int jump_search(int *array, size_t size, int value);
-int interpolation_search(int *array, size_t size, int value);
-int exponential_search(int *array, size_t size, int value);
-int advanced_binary(int *array, size_t size, int value);
-
 /**
  * struct listint_s - singly linked list
  *
@@ -28,6 +21,12 @@ size_t index;
 struct listint_s *next;
 } listint_t;
 
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 
 /**
